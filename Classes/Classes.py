43724cf685dce000bde9f53e0aa64023623b6f52
Josiah <spilkerj@bzz.ch>
2024-03-27 11:07:05 +0100

    choose your character*

--- Classes/Classes.py
+++ Classes/Classes.py
@@ -1,43 +1,130 @@
-class Character():
-    def __init__(self, ID, name, playerClass):
+class Character:
+    def __init__(self, ID, name, player_class):
         self._ID = ID
         self._name = name
-        self._playerClass = playerClass
+        self._player_class = player_class
         self._items = []
 
+    @property
+    def ID(self):
+        return self._ID
 
-class PlayerClass():
-    def __init__(self, ID, type, stats, attributes, weaponType):
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def player_class(self):
+        return self._player_class
+
+    @property
+    def items(self):
+        return self._items
+
+
+
+class PlayerClass:
+    def __init__(self, ID, type, stats, attributes, weapon_type):
         self._ID = ID
         self._type = type
         self._stats = stats
         self._attributes = attributes
-        self._weaponType = weaponType
+        self._weapon_type = weapon_type
+
+    @property
+    def ID(self):
+        return self._ID
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def stats(self):
+        return self._stats
+
+    @property
+    def attributes(self):
+        return self._attributes
+
+    @property
+    def weapon_type(self):
+        return self._weapon_type
 
 
-class WeaponType():
+class WeaponType:
     def __init__(self, ID, type, damage, description):
-        self._id = ID
+        self._ID = ID
         self._type = type
         self._damage = damage
         self._description = description
 
-        @property
-        def type(self):
-            return self._type
-        
-        @type.setter
-        def set_type(self, value):
-            sel
-            pass
+    @property
+    def ID(self):
+        return self._ID
 
-class Stats():
-    def __init__(self, ID, maxHealth, health, defense, maxMana, mana):
-        self._id = ID
-        self._type = type
-        self._maxHealth = maxHealth
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def damage(self):
+        return self._damage
+
+    @property
+    def description(self):
+        return self._description
+
+
+class Stats:
+    def __init__(self, ID, max_health, health, defense, max_mana, mana):
+        self._ID = ID
+        self._max_health = max_health
         self._health = health
         self._defense = defense
-        self._maxMana = maxMana
+        self._max_mana = max_mana
         self._mana = mana
 
+    @property
+    def ID(self):
+        return self._ID
+
+    @property
+    def max_health(self):
+        return self._max_health
+
+    @property
+    def health(self):
+        return self._health
+
+    @property
+    def defense(self):
+        return self._defense
+
+    @property
+    def max_mana(self):
+        return self._max_mana
+
+    @property
+    def mana(self):
+        return self._mana
+
+def choose_character(characters):
+    print("Available Classes:")
+    for index, character in enumerate(characters):
+        print(f"{index + 1}. ({character.player_class.type})")
+
+    while True:
+        choice = input("Choose a character by entering its number: ")
+        try:
+            choice = int(choice)
+            if 1 <= choice <= len(characters):
+                return characters[choice - 1]
+            else:
+                print("Invalid choice. Please enter a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from Classes.Classes import *
 
-# print("welcome message")
+# print welcome message
 # print lore
 # character creation:
-print("who are you?")
-def main():
-    BasicAHHSword = WeaponType(1, "Sword", 6, "Basic ass Sword" )
-    print(BasicAHHSword.type)
 
+def main():
+    print("lore, backstory, etc...")
+    print("who are you?")
+    warrior_class = PlayerClass(1, "Warrior", None, None, "Sword")
+    mage_class = PlayerClass(2, "Mage", None, None, "staff")
+    rogue_class = PlayerClass(3, "Rogue", None, None, "Dagger")
+
+    warrior = Character(1, None, warrior_class)
+    mage = Character(2, None, mage_class)
+    rogue = Character(2, None, rogue_class)
+
+    characters = [warrior, mage, rogue]
+    chosen_character = choose_character(characters)
+    chosen_character.name = input("Name and occupation? (without the occupation): ")
+    print(f"This you? {chosen_character.name} ({chosen_character.player_class.type})")
 if __name__ == "__main__":
     main()
