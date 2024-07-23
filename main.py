47946345ed0b7a8aa98dc248e71075c8bb2baa8d
Josiah <spilkerj@bzz.ch>
2024-07-23 12:14:22 +0200

    encounters works again

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import json
+import random
 from Classes.Classes import *
-
 
 def load_json(filename):
     with open(filename, 'r') as file:
         return json.load(file)
-
 
 def create_player_class(class_data):
     return PlayerClass(
@@ -16,14 +15,12 @@ def create_player_class(class_data):
         class_data['weapon_type']
     )
 
-
 def create_item(item_data):
     return {
         'ID': item_data['ID'],
         'name': item_data['name'],
         'description': item_data['description']
     }
-
 
 def get_map_size():
     while True:
@@ -37,7 +34,6 @@ def get_map_size():
         else:
             print("Invalid input. Please enter 'small', 'medium', or 'big'.")
 
-
 def main():
     try:
         # Get map size from user
@@ -45,7 +41,7 @@ def main():
         game_map = GameMap(1, map_width, map_height)
         game_map.shuffle_tiles()
 
-        encounters = load_json('encounters.json')['encounters']
+        encounters = load_json('Data/encounters.json')['encounters']
         encounter_positions = random.sample([(x, y) for x in range(map_width) for y in range(map_height)],
                                             len(encounters))
 
@@ -59,7 +55,7 @@ def main():
             )
             game_map.set_encounter(position[0], position[1], encounter)
 
-        classes = load_json('classes.json')['classes']
+        classes = load_json('Data/classes.json')['classes']
         warrior_class = create_player_class(classes[0])
         mage_class = create_player_class(classes[1])
         rogue_class = create_player_class(classes[2])
@@ -72,68 +68,64 @@ def main():
         rogue = Character(3, "", rogue_class)
 
         def choose_character(characters):
-            try:
-                print("Available Classes:")
-                for index, character in enumerate(characters):
-                    print(f"{index + 1}. ({character.player_class.type})")
+            print("Available Classes:")
+            for index, character in enumerate(characters):
+                print(f"{index + 1}. ({character.player_class.type})")
 
-                while True:
-                    choice = input("Choose a character by entering its number: ")
-                    try:
-                        choice = int(choice)
-                        if 1 <= choice <= len(characters):
-                            return characters[choice - 1]
-                        else:
-                            print("Invalid choice. Please enter a valid number.")
-                    except ValueError:
-                        print("Invalid input. Please enter a number.")
-            except Exception as e:
-                print(f"Error during character selection: {e}")
-                return None
+            while True:
+                choice = input("Choose a character by entering its number: ")
+                try:
+                    choice = int(choice)
+                    if 1 <= choice <= len(characters):
+                        return characters[choice - 1]
+                    else:
+                        print("Invalid choice. Please enter a valid number.")
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
 
         characters = [warrior, mage, rogue]
         chosen_character = choose_character(characters)
-        if chosen_character:
-            chosen_character.name = input("Name: ")
+        chosen_character.name = input("Name: ")
 
-            print(f"This you? \n{chosen_character}")
-            for stat, value in chosen_character.player_class.stats.items():
-                print(f"{stat.capitalize()}: {value}")
+        print(f"This you? \n{chosen_character}")
+        for stat, value in chosen_character.player_class.stats.items():
+            print(f"{stat.capitalize()}: {value}")
 
-            current_location = (0, 0)
+        current_location = (0, 0)
 
-            while True:
-                print(f"You are at location {current_location}")
-                direction = input("Enter direction (up, down, right, left): ")
-                new_location = current_location
+        while True:
+            print(f"You are at location {current_location}")
+            direction = input("Enter direction (up, down, right, left): ").lower()
+            new_location = current_location
 
-                if direction in ["up", "down", "right", "left"]:
-                    if direction == "up" and current_location[1] < map_height - 1:
-                        new_location = (current_location[0], current_location[1] + 1)
-                    elif direction == "down" and current_location[1] > 0:
-                        new_location = (current_location[0], current_location[1] - 1)
-                    elif direction == "right" and current_location[0] < map_width - 1:
-                        new_location = (current_location[0] + 1, current_location[1])
-                    elif direction == "left" and current_location[0] > 0:
-                        new_location = (current_location[0] - 1, current_location[1])
-                    else:
-                        print("Out of bounds.")
+            if direction in ["up", "down", "right", "left"]:
+                if direction == "up" and current_location[1] < map_height - 1:
+                    new_location = (current_location[0], current_location[1] + 1)
+                elif direction == "down" and current_location[1] > 0:
+                    new_location = (current_location[0], current_location[1] - 1)
+                elif direction == "right" and current_location[0] < map_width - 1:
+                    new_location = (current_location[0] + 1, current_location[1])
+                elif direction == "left" and current_location[0] > 0:
+                    new_location = (current_location[0] - 1, current_location[1])
                 else:
-                    print("Enter a valid direction.")
+                    print("Out of bounds.")
+            else:
+                print("Enter a valid direction.")
 
-                if new_location != current_location:
-                    current_location = new_location
+            if new_location != current_location:
+                current_location = new_location
 
+            try:
                 encounter = game_map.get_encounter(*current_location)
                 if encounter:
                     print(f"{encounter.dialog}")
                     if encounter.reward:
                         chosen_character.add_item(encounter.reward)
-                        print(f"You received: {encounter.reward['name']}")
+            except Exception as e:
+                print(f"An error occurred while processing the encounter: {e}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
-
 
 if __name__ == "__main__":
     main()
